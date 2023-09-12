00cf8c6a23b010fd8204fad72203b2b4e67575bf
DMakiz <142487178+DMakiz@users.noreply.github.com>
2023-09-12 22:20:29 +0300

    Update uidw.py

--- uidw.py
+++ uidw.py
@@ -1,3 +1,4 @@
 import subprocess
 command = "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/t2iadapter_style_sd14v1.pth -d /content/ui/extensions/sd-webui-controlnet/models -o t2iadapter_style_sd14v1.pth"
+command = "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/t2iadapter_style_sd14v1.pth -d /content/ui/extensions/sd-webui-controlnet/models -o t2iadapter_style_sd14v1.pth"
 subprocess.run(command, shell=True)
