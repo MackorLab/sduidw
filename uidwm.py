a1772f5175962b1a4452e23b814a19b1ebb73e4f
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-09-13 02:04:49 +0300

    Update uidwm.py

--- uidwm.py
+++ uidwm.py
@@ -1,1 +1,11 @@
+/content/ui/diamon/..
 # Указываем путь к целевой папке 2
+folder_path = "/content/ui/extensions"
+# Создаем папку с именем репозитория
+repo_folder_name = "batchlinks-webui"
+subprocess.run(["mkdir", f"{folder_path}/{repo_folder_name}"])
+# Клонируем репозиторий в созданную папку
+repo_url = "https://github.com/etherealxx/batchlinks-webui.git"
+subprocess.run(["git", "clone", repo_url, f"{folder_path}/{repo_folder_name}"])
+
+
