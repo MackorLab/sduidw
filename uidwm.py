ae2fb693c233edcbe7191f697d5ae5754da1b687
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-09-13 19:50:46 +0300

    Update uidwm.py

--- uidwm.py
+++ uidwm.py
@@ -6,25 +6,4 @@ os.makedirs(folder_path1, exist_ok=True)  # Создать папку
 repo_url = "https://github.com/etherealxx/batchlinks-webui.git"
 subprocess.run(["git", "clone", repo_url, folder_path1])  # Клонировать репозиторий
 
-current_dir = os.getcwd()  # Получить текущую директорию
-folder_path2 = os.path.join(current_dir, "extensions", "openpose-editor")  # Создать путь к новой папке
-os.makedirs(folder_path2, exist_ok=True)  # Создать папку
-repo_url = "https://github.com/fkunn1326/openpose-editor.git"
-subprocess.run(["git", "clone", repo_url, folder_path2])  # Клонировать репозиторий
-
-
-current_dir = os.getcwd()  # Получить текущую директорию
-folder_path3 = os.path.join(current_dir, "extensions", "sd-webui-depth-lib")  # Создать путь к новой папке
-os.makedirs(folder_path3, exist_ok=True)  # Создать папку
-repo_url = "https://github.com/AuthorSultra/sd-webui-depth-lib-Clone.git"
-subprocess.run(["git", "clone", repo_url, folder_path3])  # Клонировать репозиторий
-
-
-current_dir = os.getcwd()  # Получить текущую директорию
-folder_path4 = os.path.join(current_dir, "extensions", "sd-webui-3d-open-pose-editor")  # Создать путь к новой папке
-os.makedirs(folder_path4, exist_ok=True)  # Создать папку
-repo_url = "https://github.com/nonnonstop/sd-webui-3d-open-pose-editor.git"
-subprocess.run(["git", "clone", repo_url, folder_path4])  # Клонировать репозиторий
-
-
 
