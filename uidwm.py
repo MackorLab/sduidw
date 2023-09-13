061c6f2357e9501b716be8f4bef8b8dab6d9a522
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-09-13 17:22:31 +0300

    Update uidwm.py

--- uidwm.py
+++ uidwm.py
@@ -9,5 +9,5 @@ subprocess.run(["git", "clone", repo_url, folder_path1])  # Клонироват
 current_dir = os.getcwd()  # Получить текущую директорию
 folder_path2 = os.path.join(current_dir, "extensions", "sd-webui-depth-lib")  # Создать путь к новой папке
 os.makedirs(folder_path2, exist_ok=True)  # Создать папку
-repo_url = "https://github.com/jexom/sd-webui-depth-lib.git"
+repo_url = "https://github.com/jexom/sd-webui-depth-lib"
 subprocess.run(["git", "clone", repo_url, folder_path2])  # Клонировать репозиторий
