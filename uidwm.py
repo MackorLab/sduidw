16d7c3f8d01691069c6b21fd8a1266d29e4155d8
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-09-13 20:01:56 +0300

    Update uidwm.py

--- uidwm.py
+++ uidwm.py
@@ -1,9 +1,8 @@
 import os
 import subprocess
 current_dir = os.getcwd()  # Получить текущую директорию
-folder_path1 = os.path.join(current_dir, "extensions", "batchlinks-webui")  # Создать путь к новой папке
-os.makedirs(folder_path1, exist_ok=True)  # Создать папку
-repo_url = "https://github.com/etherealxx/batchlinks-webui.git"
-subprocess.run(["git", "clone", repo_url, folder_path1])  # Клонировать репозиторий
-
+folder_path2 = os.path.join(current_dir, "extensions", "openpose-editor")  # Создать путь к новой папке
+os.makedirs(folder_path2, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/fkunn1326/openpose-editor.git"
+subprocess.run(["git", "clone", repo_url, folder_path2])  # Клонировать репозиторий
 
