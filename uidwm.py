e1cf281ab024d177ed53fc06fc5f5430640cdcdd
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-09-13 17:08:24 +0300

    Update uidwm.py

--- uidwm.py
+++ uidwm.py
@@ -1,9 +1,13 @@
 import os
 import subprocess
 current_dir = os.getcwd()  # Получить текущую директорию
-folder_path = os.path.join(current_dir, "extensions", "batchlinks-webui")  # Создать путь к новой папке
-os.makedirs(folder_path, exist_ok=True)  # Создать папку
+folder_path1 = os.path.join(current_dir, "extensions", "batchlinks-webui")  # Создать путь к новой папке
+os.makedirs(folder_path1, exist_ok=True)  # Создать папку
 repo_url = "https://github.com/etherealxx/batchlinks-webui.git"
-subprocess.run(["git", "clone", repo_url, folder_path])  # Клонировать репозиторий
+subprocess.run(["git", "clone", repo_url, folder_path1])  # Клонировать репозиторий
 
-
+current_dir = os.getcwd()  # Получить текущую директорию
+folder_path2 = os.path.join(current_dir, "extensions", "sd-webui-depth-lib")  # Создать путь к новой папке
+os.makedirs(folder_path2, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/jexom/sd-webui-depth-lib.git"
+subprocess.run(["git", "clone", repo_url, folder_path2])  # Клонировать репозиторий
