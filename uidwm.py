bca82797d57cd911bc53ba2d7c2079c5ed3acfd6
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-09-14 22:40:40 +0300

    Update uidwm.py

--- uidwm.py
+++ uidwm.py
@@ -24,3 +24,35 @@ repo_url3 = "https://huggingface.co/datasets/DmatryMakeev/SadTalker"
 subprocess.run(["git", "clone", repo_url3, checkpoints_path])  # Клонировать репозиторий
 
 
+current_dir = os.getcwd()  # Получить текущую директорию
+folder_path3 = os.path.join(current_dir, "extensions", "stable-diffusion-webui-rembg")  # Создать путь к новой папке
+os.makedirs(folder_path3, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/AUTOMATIC1111/stable-diffusion-webui-rembg.git"
+subprocess.run(["git", "clone", repo_url, folder_path3])  # Клонировать репозиторий
+
+
+current_dir = os.getcwd()  # Получить текущую директорию
+folder_path4 = os.path.join(current_dir, "extensions", "stable-diffusion-webui-two-shot")  # Создать путь к новой папке
+os.makedirs(folder_path4, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/ashen-sensored/stable-diffusion-webui-two-shot.git"
+subprocess.run(["git", "clone", repo_url, folder_path4])  # Клонировать репозиторий
+
+
+current_dir = os.getcwd()  # Получить текущую директорию
+folder_path5 = os.path.join(current_dir, "extensions", "sd-webui-aspect-ratio-helper")  # Создать путь к новой папке
+os.makedirs(folder_path5, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/thomasasfk/sd-webui-aspect-ratio-helper.git"
+subprocess.run(["git", "clone", repo_url, folder_path5])  # Клонировать репозиторий
+
+current_dir = os.getcwd()  # Получить текущую директорию
+folder_path6 = os.path.join(current_dir, "extensions", "asymmetric-tiling-sd-webui")  # Создать путь к новой папке
+os.makedirs(folder_path6, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/tjm35/asymmetric-tiling-sd-webui.git"
+subprocess.run(["git", "clone", repo_url, folder_path6])  # Клонировать репозиторий
+
+current_dir = os.getcwd()  # Получить текущую директорию
+folder_path7 = os.path.join(current_dir, "extensions", "posex")  # Создать путь к новой папке
+os.makedirs(folder_path7, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/hnmr293/posex.git"
+subprocess.run(["git", "clone", repo_url, folder_path7])  # Клонировать репозиторий
+
