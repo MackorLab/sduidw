c0737b1556be11104e90fa77ff355864868251cd
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-09-14 12:20:22 +0300

    Update uidwm.py

--- uidwm.py
+++ uidwm.py
@@ -13,10 +13,14 @@ repo_url = "https://github.com/fkunn1326/openpose-editor.git"
 subprocess.run(["git", "clone", repo_url, folder_path2])  # Клонировать репозиторий
 
 
+# Создание папки и клонирование репозитория для папки "checkpoints"
 current_dir = os.getcwd()  # Получить текущую директорию
-folder_path3 = os.path.join(current_dir, "checkpoints")  # Создать путь к новой папке
-os.makedirs(folder_path3, exist_ok=True)  # Создать папку
-repo_url = "https://huggingface.co/camenduru/SadTalker"
-subprocess.run(["git", "clone", repo_url, folder_path3])  # Клонировать репозиторий
+extensions_path = os.path.join(current_dir, "extensions")  # Путь к папке "extensions"
+sadtalker_path = os.path.join(extensions_path, "SadTalker")  # Путь к папке "SadTalker"
+os.makedirs(sadtalker_path, exist_ok=True)  # Создать папку "SadTalker"
+checkpoints_path = os.path.join(sadtalker_path, "checkpoints")  # Путь к папке "checkpoints"
+os.makedirs(checkpoints_path, exist_ok=True)  # Создать папку "checkpoints"
+repo_url3 = "https://huggingface.co/camenduru/SadTalker"
+subprocess.run(["git", "clone", repo_url3, checkpoints_path])  # Клонировать репозиторий
 
 
