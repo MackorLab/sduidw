cf85ed10b1ffdc568c89d90686339715f8f4d10b
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-09-15 16:30:29 +0300

    Update uidwm.py

--- uidwm.py
+++ uidwm.py
@@ -47,6 +47,14 @@ subprocess.run(["git", "clone", repo_url, folder_path7])  # Клонироват
 
 
 
-
+# Создание папки и клонирование репозитория для папки "checkpoints"
+current_dir = os.getcwd()  # Получить текущую директорию
+extensions_path = os.path.join(current_dir, "extensions")  # Путь к папке "extensions"
+sadtalker_path = os.path.join(extensions_path, "SadTalker")  # Путь к папке "SadTalker"
+os.makedirs(sadtalker_path, exist_ok=True)  # Создать папку "SadTalker"
+checkpoints_path = os.path.join(sadtalker_path, "checkpoints")  # Путь к папке "checkpoints"
+os.makedirs(checkpoints_path, exist_ok=True)  # Создать папку "checkpoints"
+repo_url3 = "https://huggingface.co/datasets/DmatryMakeev/SadTalker"
+subprocess.run(["git", "clone", repo_url3, checkpoints_path])  # Клонировать репозиторий
 
 
