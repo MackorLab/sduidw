c29472539c22f4ef60c2350554a309eee40438cc
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-11-09 02:33:43 +0300

    Update uidwms.py

--- uidwms.py
+++ uidwms.py
@@ -63,9 +63,9 @@ subprocess.run(["git", "clone", repo_url, folder_path9])  # Клонироват
 
 
 current_dir = os.getcwd()  # Получить текущую директорию
-folder_path10 = os.path.join(current_dir, "extensions", "sd-webui-faceswaplab")  # Создать путь к новой папке
+folder_path10 = os.path.join(current_dir, "extensions", "deforum-for-automatic1111-webui")  # Создать путь к новой папке
 os.makedirs(folder_path10, exist_ok=True)  # Создать папку
-repo_url = "https://github.com/glucauze/sd-webui-faceswaplab.git"
+repo_url = "https://github.com/deforum-art/deforum-for-automatic1111-webui.git"
 subprocess.run(["git", "clone", repo_url, folder_path10])  # Клонировать репозиторий
 
 
