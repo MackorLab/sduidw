2b17787dde8daf0866d2fe12e236f12b6fdd6529
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-11-08 23:32:31 +0300

    Update kag_uidw.py

--- kag_uidw.py
+++ kag_uidw.py
@@ -1,7 +1,6 @@
 import subprocess
 commands = [
-    "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/DmitrMakeev/DiamonSD-Adapter/resolve/main/ip-adapter-plus-face_sd15.pth -d /kaggle/working/stable-diffusion-webui/extensions/sd-webui-controlnet/models -o ip-adapter-plus-face_sd15.pth
-",
+    "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/DmitrMakeev/DiamonSD-Adapter/resolve/main/ip-adapter-plus-face_sd15.pth -d /kaggle/working/stable-diffusion-webui/extensions/sd-webui-controlnet/models -o ip-adapter-plus-face_sd15.pth",
    
 ]
 for command in commands:
