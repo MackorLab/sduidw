bf25860efe9bea412441e7e5999d471b7e345bb2
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-11-09 20:19:21 +0300

    Update kag_uidw.py

--- kag_uidw.py
+++ kag_uidw.py
@@ -2,6 +2,7 @@ import subprocess
 commands = [
     "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/DmitrMakeev/DiamonSD-Adapter/resolve/main/ip-adapter-plus-face_sd15.pth -d /kaggle/working/stable-diffusion-webui/extensions/sd-webui-controlnet/models -o ip-adapter-plus-face_sd15.pth",
     "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v15_v2.ckpt -d /kaggle/working/stable-diffusion-webui/extensions/sd-webui-animatediff/model -o mm_sd_v15_v2.ckpt",
+    "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/guoyww/animatediff/resolve/main/v2_lora_PanLeft.ckpt -d /kaggle/working/stable-diffusion-webui/models/Lora/AnimateDiff -o mm_sd_v15_v2.ckpt",
 ]
 for command in commands:
     subprocess.run(command, shell=True)
