92d0c2681176ed1d9f4e9435109a8b4829f1ccac
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-11-09 11:51:36 +0300

    Update uidwms.py

--- uidwms.py
+++ uidwms.py
@@ -80,7 +80,7 @@ subprocess.run(["git", "clone", repo_url, folder_path11])  # Клонирова�
 #current_dir = os.getcwd()  # Получить текущую директорию
 #folder_path12 = os.path.join(current_dir, "extensions", "stable-diffusion-webui-prompt-travel")  # Создать путь к новой папке
 #os.makedirs(folder_path12, exist_ok=True)  # Создать папку
-#repo_url12 = "https://github.com/s9roll7/animatediff-cli-prompt-travel.git"
+#repo_url12 = "https://github.com/Kahsolt/stable-diffusion-webui-prompt-travel.git"
 #subprocess.run(["git", "clone", repo_url12, folder_path12])  # Клонировать репозиторий
 
 
