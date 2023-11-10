1bf2d854f7572f6f5105223c8477058c06563be3
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-11-10 07:52:10 +0300

    Update kag_uidw.py

--- kag_uidw.py
+++ kag_uidw.py
@@ -16,7 +16,7 @@ commands = [
     "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/guoyww/animatediff/resolve/main/v2_lora_PanLeft.ckpt -d /kaggle/working/stable-diffusion-webui/models/Lora/AnimateDiff -o v2_lora_PanLeft.ckpt",
 
 
-   "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://civitai.com/api/download/models/218017?type=Model&format=SafeTensor&size=pruned&fp=fp16 -d /kaggle/working/stable-diffusion-webui/models/Stable-diffusion -o realcartoon3d_v10.safetensors",
+   "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/DmitrMakeev/Models-coll/resolve/main/models/realcartoon3d_v10.safetensors -d /kaggle/working/stable-diffusion-webui/models/Stable-diffusion -o realcartoon3d_v10.safetensors",
 
 
 
