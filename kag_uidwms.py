d338d89f995d40016fcf341639a5953dd05d00ed
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-11-10 22:10:25 +0300

    Update kag_uidwms.py

--- kag_uidwms.py
+++ kag_uidwms.py
@@ -109,10 +109,5 @@ folder_path16 = os.path.join(current_dir16, "models", "Lora", "AnimateDiff")  # 
 os.makedirs(folder_path16, exist_ok=True)  # Создать папку
 
 
-current_dir = os.getcwd()  # Получить текущую директорию
-folder_path17 = os.path.join(current_dir, "extensions", "Stable-Diffusion-WebUI-TensorRT")  # Создать путь к новой папке
-os.makedirs(folder_path17, exist_ok=True)  # Создать папку
-repo_url = "https://github.com/NVIDIA/Stable-Diffusion-WebUI-TensorRT.git"
-subprocess.run(["git", "clone", repo_url, folder_path17])  # Клонировать репозиторий
 
 
