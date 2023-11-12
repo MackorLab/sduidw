4a33b04bd2d91d2cab81cf7a8274b3d36d1fd9ce
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-11-13 00:35:55 +0300

    Update kag_uid_vid_defor.py

--- kag_uid_vid_defor.py
+++ kag_uid_vid_defor.py
@@ -1,1 +1,89 @@
-vid
+import os
+import subprocess
+
+
+
+
+
+current_dir = os.getcwd()  # Получить текущую директорию
+folder_path3 = os.path.join(current_dir, "extensions", "stable-diffusion-webui-rembg")  # Создать путь к новой папке
+os.makedirs(folder_path3, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/AUTOMATIC1111/stable-diffusion-webui-rembg.git"
+subprocess.run(["git", "clone", repo_url, folder_path3])  # Клонировать репозиторий
+
+
+current_dir = os.getcwd()  # Получить текущую директорию
+folder_path4 = os.path.join(current_dir, "extensions", "stable-diffusion-webui-two-shot")  # Создать путь к новой папке
+os.makedirs(folder_path4, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/ashen-sensored/stable-diffusion-webui-two-shot.git"
+subprocess.run(["git", "clone", repo_url, folder_path4])  # Клонировать репозиторий
+
+
+current_dir = os.getcwd()  # Получить текущую директорию
+folder_path5 = os.path.join(current_dir, "extensions", "sd-webui-aspect-ratio-helper")  # Создать путь к новой папке
+os.makedirs(folder_path5, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/thomasasfk/sd-webui-aspect-ratio-helper.git"
+subprocess.run(["git", "clone", repo_url, folder_path5])  # Клонировать репозиторий
+
+
+
+current_dir = os.getcwd()  # Получить текущую директорию
+folder_path7 = os.path.join(current_dir, "extensions", "posex")  # Создать путь к новой папке
+os.makedirs(folder_path7, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/hnmr293/posex.git"
+subprocess.run(["git", "clone", repo_url, folder_path7])  # Клонировать репозиторий
+
+
+
+current_dir = os.getcwd()  # Получить текущую директорию
+folder_path8 = os.path.join(current_dir, "extensions", "sd-webui-controlnet")  # Создать путь к новой папке
+os.makedirs(folder_path8, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/Mikubill/sd-webui-controlnet.git"
+subprocess.run(["git", "clone", repo_url, folder_path8])  # Клонировать репозиторий
+
+
+
+
+
+current_dir = os.getcwd()  # Получить текущую директорию
+folder_path10 = os.path.join(current_dir, "extensions", "deforum-for-automatic1111-webui")  # Создать путь к новой папке
+os.makedirs(folder_path10, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/deforum-art/deforum-for-automatic1111-webui.git"
+subprocess.run(["git", "clone", repo_url, folder_path10])  # Клонировать репозиторий
+
+
+
+current_dir = os.getcwd()  # Получить текущую директорию
+folder_path11 = os.path.join(current_dir, "extensions", "depthmap2mask")  # Создать путь к новой папке
+os.makedirs(folder_path11, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/Extraltodeus/depthmap2mask.git"
+subprocess.run(["git", "clone", repo_url, folder_path11])  # Клонировать репозиторий
+
+
+#current_dir = os.getcwd()  # Получить текущую директорию
+#folder_path12 = os.path.join(current_dir, "extensions", "stable-diffusion-webui-prompt-travel")  # Создать путь к новой папке
+#os.makedirs(folder_path12, exist_ok=True)  # Создать папку
+#repo_url12 = "https://github.com/Kahsolt/stable-diffusion-webui-prompt-travel.git"
+#subprocess.run(["git", "clone", repo_url12, folder_path12])  # Клонировать репозиторий
+
+
+
+current_dir = os.getcwd()  # Получить текущую директорию
+folder_path15 = os.path.join(current_dir, "extensions", "stable-diffusion-webui-dataset-tag-editor")  # Создать путь к новой папке
+os.makedirs(folder_path15, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/toshiaki1729/stable-diffusion-webui-dataset-tag-editor.git"
+subprocess.run(["git", "clone", repo_url, folder_path15])  # Клонировать репозиторий
+
+
+current_dir = os.getcwd()  # Получить текущую директорию
+folder_path17 = os.path.join(current_dir, "extensions", "sd-webui-prompt-all-in-one")  # Создать путь к новой папке
+os.makedirs(folder_path17, exist_ok=True)  # Создать папку
+repo_url = "https://github.com/Physton/sd-webui-prompt-all-in-one.git"
+subprocess.run(["git", "clone", repo_url, folder_path17])  # Клонировать репозиторий
+
+
+
+
+current_dir16 = os.getcwd()  # Получить текущую директорию
+folder_path16 = os.path.join(current_dir16, "models", "Lora", "AnimateDiff")  # Создать путь к новой папке
+os.makedirs(folder_path16, exist_ok=True)  # Создать папку
