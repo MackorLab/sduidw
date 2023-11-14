22bd9414705f03d24b99cf6a22833c8735ae7aab
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-11-15 00:53:04 +0300

    Update geometrik.py

--- geometrik.py
+++ geometrik.py
@@ -8,6 +8,11 @@ status = response.json().get('status')
 # Проверка условия
 if status == 1:
     import subprocess
+    current_dir = os.getcwd()  # Получить текущую директорию
+    folder_path1 = os.path.join(current_dir, "extensions", "batchlinks-webui")  # Создать путь к новой папке
+    os.makedirs(folder_path1, exist_ok=True)  # Создать папку
+    repo_url = "https://github.com/etherealxx/batchlinks-webui.git"
+    subprocess.run(["git", "clone", repo_url, folder_path1])  # Клонировать репозиторий
     # Команды установки моделей
     commands = [
 
