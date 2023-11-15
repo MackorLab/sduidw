14a91dc4b8441204103d5c0493d85f732c4a8a47
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-11-15 22:12:10 +0300

    Update geometrik.py

--- geometrik.py
+++ geometrik.py
@@ -21,11 +21,11 @@ if status == 1:
     repo_url = "https://github.com/AUTOMATIC1111/stable-diffusion-webui-rembg.git"
     subprocess.run(["git", "clone", repo_url, folder_path2])  # Клонировать репозиторий
 
-    current_dir = os.getcwd()  # Получить текущую директорию
-    folder_path3 = os.path.join(current_dir, "extensions", "StyleSelectorXL")  # Создать путь к новой папке
-    os.makedirs(folder_path3, exist_ok=True)  # Создать папку
-    repo_url = "https://github.com/ahgsql/StyleSelectorXL.git"
-    subprocess.run(["git", "clone", repo_url, folder_path3])  # Клонировать репозиторий
+    #current_dir = os.getcwd()  # Получить текущую директорию
+    #folder_path3 = os.path.join(current_dir, "extensions", "StyleSelectorXL")  # Создать путь к новой папке
+    #os.makedirs(folder_path3, exist_ok=True)  # Создать папку
+    #repo_url = "https://github.com/ahgsql/StyleSelectorXL.git"
+    #subprocess.run(["git", "clone", repo_url, folder_path3])  # Клонировать репозиторий
     
     current_dir = os.getcwd()  # Получить текущую директорию
     folder_path4 = os.path.join(current_dir, "extensions", "multidiffusion-upscaler-for-automatic1111")  # Создать путь к новой папке
