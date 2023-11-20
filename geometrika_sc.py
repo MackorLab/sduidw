6de8c37a6b24d777d590ce41e6ec6a48a751a4b8
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-11-20 12:59:42 +0300

    Update geometrika_sc.py

--- geometrika_sc.py
+++ geometrika_sc.py
@@ -80,11 +80,11 @@ if status == 1:
     #subprocess.run(["git", "clone", repo_url, folder_path18])  # Клонировать репозиторий
 
 
-    #current_dir = os.getcwd()  # Получить текущую директорию
-    #folder_path19 = os.path.join(current_dir, "extensions", "next-view")  # Создать путь к новой папке
-    #os.makedirs(folder_path19, exist_ok=True)  # Создать папку
-    #repo_url = "https://github.com/NextDiffusion/next-view"
-    #subprocess.run(["git", "clone", repo_url, folder_path19])  # Клонировать репозиторий
+    current_dir = os.getcwd()  # Получить текущую директорию
+    folder_path19 = os.path.join(current_dir, "extensions", "next-view")  # Создать путь к новой папке
+    os.makedirs(folder_path19, exist_ok=True)  # Создать папку
+    repo_url = "https://github.com/NextDiffusion/next-view"
+    subprocess.run(["git", "clone", repo_url, folder_path19])  # Клонировать репозиторий
 
     
     # Команды установки моделей
