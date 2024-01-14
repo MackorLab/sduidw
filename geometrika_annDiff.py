682915b3bec07754b491e6ae6764f7a822918525
MackorLab <76862637+MackorLab@users.noreply.github.com>
2024-01-14 23:58:55 +0300

    Update geometrika_annDiff.py

--- geometrika_annDiff.py
+++ geometrika_annDiff.py
@@ -65,6 +65,14 @@ if status == 1:
     subprocess.run(["git", "clone", repo_url, folder_path17])  # Клонировать репозиторий
 
 
+    current_dir = os.getcwd()  # Получить текущую директорию
+    folder_path18 = os.path.join(current_dir, "extensions", "sd-webui-reactor")  # Создать путь к новой папке
+    os.makedirs(folder_path18, exist_ok=True)  # Создать папку
+    repo_url = "https://github.com/Gourieff/sd-webui-reactor.git"
+    subprocess.run(["git", "clone", repo_url, folder_path18])  # Клонировать репозиторий
+
+
+    
     current_dir16 = os.getcwd()  # Получить текущую директорию
     folder_path16 = os.path.join(current_dir16, "models", "Lora", "AnimateDiff")  # Создать путь к новой папке
     os.makedirs(folder_path16, exist_ok=True)  # Создать папку
